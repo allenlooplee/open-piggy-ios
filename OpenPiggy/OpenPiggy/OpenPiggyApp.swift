fc1e72b2fca3ea8f09f450cb2a46e2899a53d0f4
Allen Lee <looplee@msn.com>
2024-04-25 20:51:52 +0800

    Create the app with iOS project template

---
+++ OpenPiggy/OpenPiggy/OpenPiggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPiggyApp.swift
+//  OpenPiggy
+//
+//  Created by Allen Lee on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenPiggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
